26334abcb29174680961f9bc67ed933f67bbce5f
ValeriyKliuk <valeriy.kliuk@gmail.com>
2024-04-11 00:10:02 -0400

    Added axios to get Products from .NET API

--- products-client-app/src/App.tsx
+++ products-client-app/src/App.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return <h1>Products</h1>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/Products').then((response) => {
+      console.log(response.data);
+      setProducts(response.data);
+    });
+  }, []);
+  return (
+    <div>
+      <h1>Products</h1>
+      {products.map((product: any) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <p>{product.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
